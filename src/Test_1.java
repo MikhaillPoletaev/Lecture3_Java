539cf619f7997e117ec5e9ae575650afd74ba268
Mikhail <mpolet@outlook.com>
2022-10-19 14:44:00 +0300

    Initial commit

---
+++ src/Test_1.java
@@ -0,0 +1,11 @@
+public class Test_1 {
+    public static void main(String[] args) {
+        int amount = 15000; // стоимость билета
+
+        int miles = 20; // рублей за 1 милю
+
+        int bonus = amount / miles;
+
+        System.out.println("Бонус = " + (bonus) + " миль ");
+    }
+}
---
+++ src/Test_2.java
@@ -0,0 +1,21 @@
+public class Test_2 {
+    public static void main(String[] args){
+        int account = 100; // изначальный баланс
+
+        int add = 2000; // сумма пополнения
+
+        int bonus = 0;
+
+        if (add > 1000){
+            bonus = add / 100;
+        } else {
+            if (add < 1000){
+                bonus = 0;
+            }
+        }
+        int final_balance = account + add + bonus;
+        System.out.println("Баланс: " + final_balance);
+        System.out.println("Бонус: " + bonus);
+
+    }
+}
